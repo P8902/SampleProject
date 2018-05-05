64d6c39e60554e872fe0bb9fd1ffb2c8bc39df59
Prabhat Ranjan Singh <prbhatranjan2206@gmail.com>
2018-05-05 17:02:50 +0530

    My First Commit

---
+++ SampleProject/SampleProject/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleProject
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        void OnTranslate(object sender, EventArgs e)
+        {
+            DisplayAlert("Prabhat", "hello", "ok");
+        }
+    }
+}
