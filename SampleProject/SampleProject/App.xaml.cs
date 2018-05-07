ee0fd2bcad876bdf2c6c2d5b032dc47ad8f2f55a
Prabhat Ranjan Singh <prbhatranjan2206@gmail.com>
2018-05-07 17:37:35 +0530

    added appcentre header and the nugets

--- SampleProject/SampleProject/App.xaml.cs
+++ SampleProject/SampleProject/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace SampleProject
 {
@@ -18,8 +20,8 @@ namespace SampleProject
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            AppCenter.Start("android=0581d912-8d9c-4ff6-b2e7-880cb00a8cea;" +"uwp={Your UWP App secret here};" +"ios={Your iOS App secret here}",typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
